1bfbee3225872f7d40d3c91eadc00bc99bb23597
Gourav Malviya <65213810+Gourav-mlv@users.noreply.github.com>
2020-12-26 18:27:01 +0530

    Add files via upload

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const cors = require('cors');
-const upload = require('express-fileupload');
+// const upload = require('express-fileupload');
 const app = express();
+var formidable = require('formidable');
+const fs = require('fs');
 
 app.use(cors());
 app.use(express.json());
-app.use(upload());
+// app.use(upload());
 
 
 
@@ -14,85 +16,160 @@ app.get('/', (req, res) => {
     // var text = req.params.secret;
     // res.write(text);
     res.send("Hello");
-    console.log(req.params.audio);
+
 });
 
 app.post('/upload', (req, res) => {
+    var form = new formidable.IncomingForm();
+    form.parse(req, (err, fields, files) => {
+        console.log(fields);
+        console.log(files);
+
+        if (fields.SecretText != '#####') {
+            if (files) {
+                var file = files.AudioFile,
+                    filename = file.name;
+
+                var spawn = require("child_process").spawn;
+                var child = spawn('python', ["./encrypter.py",
+                    fields.SecretText,
+                    file.path,
+                    filename]);
+
+                child.stdout.on('data', function (data) {
+                    console.log(data.toString());
+                    //return res.send(data.toString());
+                })
+
+                child.stderr.on('data', function (data) {
+                    console.log(data.toString());
+                    //return res.send(data.toString());
+                })
+
+                child.stdout.on('close', () => {
+
+                    res.download("./modified/" + filename + "embedded.wav");
+                })
 
 
-    if (req.body.SecretText) {
-        if (req.files) {
-            console.log(req.body);
-            console.log(req.files);
-            var file = req.files.AudioFile,
-                filename = Math.random().toString() + file.name;
-            file.mv("./upload/" + filename, (err) => {
-                if (err) {
-                    console.log(err);
-                    res.send("error occured")
-                } else {
-                    return res.send('File uploaded');
-                }
-            })
-
-
-            var spawn = require("child_process").spawn;
-            var process = spawn('python', ["./script.py",
-                req.body.SecretText,
-                filename]);
-
-            process.stdout.on('data', function (data) {
-                console.log(data.toString());
-                return res.send(data.toString());
-            })
-
-
+            }
         }
-    } else {
-        if (req.files) {
-            console.log(req.body);
-            console.log(req.files);
+        else {
+            if (files) {
+                console.log(req.body);
+                console.log(files.AudioFile);
 
-            // res.write("Decrypt File Uploaded");
-           // res.sendStatus(200);
-
-            var file = req.files.AudioFile,
-                filename = Math.random().toString() + file.name;
-
-            file.mv("./upload/dec" + filename, (err) => {
-                if (err) {
-                    console.log(err);
-                    res.send("error occured")
-                } else {
-                    console.log('Decrypt File uploaded');
-                }
-            })
+                var file = files.AudioFile,
+                    filename = file.name;
 
 
-            var spawn = require("child_process").spawn;
-            var process = spawn('python', ["./decrypt.py",
-                filename]);
+                // console.log(req.files.AudioFile.mv.toString());
+                // fs.writeFile("./upload/dec/" +  filename,file.data, function(err) {
+                //     if(err) {
+                //         return console.log(err);
+                //     }
+                //     console.log("The file was saved!");
+                // }); 
 
+                // file.mv("./upload/dec/" + filename)/* (err) => {
+                //     if (err) {
+                //         console.log(err);
+                //         //res.send("error occured")
+                //     } else {
+                //         console.log('Decrypt File uploaded');
+                //     }
+                // })
 
+                res.writeHead(200, { 'Content-Type': 'text/html' });
 
-            process.stdout.on('data', function (data) {
-                res.send(data.toString());
-                console.log(data.toString());
-                dataToSend = data.toString();
-            })
-            // setTimeout(()=>{
-            //     return res.send(dataToSend);
-            // }, 3000);
+                var spawn = require("child_process").spawn;
+                var child = spawn('python', ["./decrypter.py",
+                    file.path]);
 
-            // process.on('close', (code) => {
-            //     console.log(`child process close all stdio with code ${code}`);
-            //     // send data to browser
-            //     return res.send(dataToSend)
-            // });
+                child.stdout.on('data', (data) => {
+                    console.log(data.toString());
+                })
 
+                child.stderr.on('data', function (data) {
+                    console.log(data.toString());
+                    //return res.send(data.toString());
+                })
+
+                child.stdout.pipe(res);
+            }
         }
-    }
+    });
+    // if (req.body.SecretText != '#####') {
 
+    // if (req.files) {
+    //     console.log(fields);
+    //     console.log(files);
+    //     var file = req.files.AudioFile,
+    //         filename = /*Math.random().toString()+  */ file.name;
+    //     // file.mv("./upload/" + filename, (err) => {
+    //     //     if (err) {
+    //     //         console.log(err);
+    //     //         res.send("error occured")
+    //     //     } else {
+    //     //         return res.send('File uploaded');
+    //     //     }
+    //     // })
+    //     var spawn = require("child_process").spawn;
+    //     var child = spawn('python', ["./script.py",
+    //         req.body.SecretText,
+    //         filename]);
+
+    //     child.stdout.on('data', function (data) {
+    //         console.log(data.toString());
+    //         //return res.send(data.toString());
+    //     })
+
+    //     child.stdout.on('close',()=>{
+    //         res.download("./modified/" +  filename+"embedded.wav");
+    //     })
+
+
+    //     }
+    // }
+    // else {
+    //     if (req.files) {
+    //         console.log(req.body);
+    //         console.log(req.files.AudioFile);
+
+    //         var file = req.files.AudioFile,
+    //             filename = /*Math.random().toString() +*/ file.name;
+
+
+    //         // console.log(req.files.AudioFile.mv.toString());
+    //         // fs.writeFile("./upload/dec/" +  filename,file.data, function(err) {
+    //         //     if(err) {
+    //         //         return console.log(err);
+    //         //     }
+    //         //     console.log("The file was saved!");
+    //         // }); 
+
+    //         // file.mv("./upload/dec/" + filename)/* (err) => {
+    //         //     if (err) {
+    //         //         console.log(err);
+    //         //         //res.send("error occured")
+    //         //     } else {
+    //         //         console.log('Decrypt File uploaded');
+    //         //     }
+    //         // })
+
+    //         res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+    //         var spawn = require("child_process").spawn;
+    //         var child = spawn('python', ["./decrypt.py",
+    //             filename]);
+
+    //         child.stdout.on('data', (data) => {
+    //             console.log(data.toString());
+    //         })
+
+    //         child.stdout.pipe(res);
+    //     }
+    // }
 
 });
 
